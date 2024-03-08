3ec5deb6de6abca0ba2df0d710fd9af3fed687cf
Aulys VINAY <aulys.n.vinay@outlook.com>
2024-03-08 20:36:28 +0100

    Install library

--- aulysv/vite.config.ts
+++ aulysv/vite.config.ts
@@ -1,6 +1,14 @@
+import path from "path"
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+
+
+
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
 });
