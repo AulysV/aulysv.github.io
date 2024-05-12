7ec0adb68a6e18e86f0c14f18f9a3c6f44318dc7
Aulys VINAY <aulys.n.vinay@outlook.com>
2024-05-12 13:19:08 +0200

    Test

--- aulys/src/routes/reims/+page.svelte
+++ aulys/src/routes/reims/+page.svelte
@@ -1,5 +1,66 @@
 <script>
   import reims from "$lib/reims.mp4";
+  import un from "$lib/images/photoreims/1.jpg";
+  import deux from "$lib/images/photoreims/2.jpg";
+  import trois from "$lib/images/photoreims/3.jpg";
+  import quatre from "$lib/images/photoreims/4.jpg";
+  import cinq from "$lib/images/photoreims/5.jpg";
+  import six from "$lib/images/photoreims/6.jpg";
+  import sept from "$lib/images/photoreims/7.jpg";
+  import huit from "$lib/images/photoreims/8.jpg";
+  import neuf from "$lib/images/photoreims/9.jpg";
+  import dix from "$lib/images/photoreims/10.jpg";
+  import onze from "$lib/images/photoreims/11.jpg";
+  import douze from "$lib/images/photoreims/12.jpg";
+  import treize from "$lib/images/photoreims/13.jpg";
+  import quatorze from "$lib/images/photoreims/14.jpg";
+  import quinze from "$lib/images/photoreims/15.jpg";
+  import seize from "$lib/images/photoreims/16.jpg";
+  import dixsept from "$lib/images/photoreims/17.jpg";
+  import dixhuit from "$lib/images/photoreims/18.jpg";
+  import dixneuf from "$lib/images/photoreims/19.jpg";
+  import vingt from "$lib/images/photoreims/20.jpg";
+  import vingtun from "$lib/images/photoreims/21.jpg";
+  import vingtdeux from "$lib/images/photoreims/22.jpg";
+  import vingttrois from "$lib/images/photoreims/23.jpg";
+  import vingtquatre from "$lib/images/photoreims/24.jpg";
+  import vingtcinq from "$lib/images/photoreims/25.jpg";
+  import vingtsix from "$lib/images/photoreims/26.jpg";
+  import vingtsept from "$lib/images/photoreims/27.jpg";
+  import vingthuit from "$lib/images/photoreims/28.jpg";
+  import vingtneuf from "$lib/images/photoreims/29.jpg";
+  import trente from "$lib/images/photoreims/30.jpg";
+  import trenteun from "$lib/images/photoreims/31.jpg";
+  import trentedeux from "$lib/images/photoreims/32.jpg";
+  import trentetrois from "$lib/images/photoreims/33.jpg";
+  import trentequatre from "$lib/images/photoreims/34.jpg";
+  import trentecinq from "$lib/images/photoreims/35.jpg";
+  import trentesix from "$lib/images/photoreims/36.jpg";
+  import trentesept from "$lib/images/photoreims/37.jpg";
+  import trentehuit from "$lib/images/photoreims/38.jpg";
+  import trenteneuf from "$lib/images/photoreims/39.jpg";
+  import quarante from "$lib/images/photoreims/40.jpg";
+  import quaranteun from "$lib/images/photoreims/41.jpg";
+  import quarantedeux from "$lib/images/photoreims/42.jpg";
+  import quarantetrois from "$lib/images/photoreims/43.jpg";
+  import quarantequatre from "$lib/images/photoreims/44.jpg";
+  import quarantecinq from "$lib/images/photoreims/45.jpg";
+  import quarantesix from "$lib/images/photoreims/46.jpg";
+  import quarantesept from "$lib/images/photoreims/47.jpg";
+  import quarantehuit from "$lib/images/photoreims/48.jpg";
+  import quaranteneuf from "$lib/images/photoreims/49.jpg";
+  import cinquante from "$lib/images/photoreims/50.jpg";
+  import cinquanteun from "$lib/images/photoreims/51.jpg";
+  import cinquantedeux from "$lib/images/photoreims/52.jpg";
+  import cinquantetrois from "$lib/images/photoreims/53.jpg";
+  import cinquantequatre from "$lib/images/photoreims/54.jpg";
+  import cinquantecinq from "$lib/images/photoreims/55.jpg";
+  import cinquantesix from "$lib/images/photoreims/56.jpg";
+  import cinquantesept from "$lib/images/photoreims/57.jpg";
+  import cinquantehuit from "$lib/images/photoreims/58.jpg";
+  import cinquanteneuf from "$lib/images/photoreims/59.jpg";
+  import soixante from "$lib/images/photoreims/60.jpg";
+  import soixanteun from "$lib/images/photoreims/61.mp4";
 </script>
 
 <div class="min-h-svh p-0">
@@ -19,22 +80,270 @@
 
   <!-- Video -->
 
-  <video
-    src={reims}
-    class="my-14"
-    controls
-    style="width: calc(100% - 50px); margin: 0 25px;"
-  ></video>
+  <video src={reims} id="video1" controls></video>
 
-  <article id="maindiv" class="prose max-w-none lg:text-xl">
-    <h2>Jour 1</h2>
-    <p>
-      Départ à 8h15, il faisait frais et brumeux, côtes dès le début, boue,
-      monsieur St Jacques, fatigue musculaire donc camping
-    </p>
-    <h2>Jour 2</h2>
-    <h2>Jour 3</h2>
-  </article>
+  <div class="my-15">
+    <h1 class="text-center font-bold text-5xl my-10">Jour 1</h1>
+    <ul
+      class="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical"
+    >
+      <!-- 1 -->
+      <li>
+        <div class="timeline-middle">
+          <i class="fa-regular fa-circle-dot text-primary text-xl"></i>
+        </div>
+        <div class="timeline-end mb-10">
+          <div class="text-lg font-black">Départ</div>
+          <p>
+            Départ en vélo à 8h15, direction Reims <br />
+            Bords de Marne jusqu'à la côte de Chessy pour me défoncer les jambes
+            <br />
+            Puis boue, il faisait frais et brumeux. <br />
+            Arrêt à Jouarre <br />
+            Monsieur St Jacques à croutes sur marne <br />
+            Fatigue musculaire <br />
+            Camping à Dormans (jeu de mot mdr) <br />
+          </p>
+        </div>
+        <div class="timeline-start mb-10">
+          <img src={un} alt="Macintosh" class="rounded-lg w-96" />
+        </div>
+        <hr />
+      </li>
+      <!-- 2 -->
+      <li>
+        <div class="timeline-middle">
+          <i class="fa-regular fa-circle-dot text-primary text-xl"></i>
+        </div>
+        <div class="timeline-end md:text-start mb-10">
+          <div class="text-lg font-black">First Macintosh computer</div>
+          The Apple Macintosh—later rebranded as the Macintosh 128K—is the original
+          Apple Macintosh personal computer. It played a pivotal role in establishing
+          desktop publishing as a general office function. The motherboard, a 9 in
+          (23 cm) CRT monitor, and a floppy drive were housed in a beige case with
+          integrated carrying handle; it came with a keyboard and single-button mouse.
+        </div>
+        <div class="timeline-start mb-10"></div>
+        <hr />
+      </li>
+
+      <!-- 3 -->
+
+      <li>
+        <div class="timeline-middle">
+          <i class="fa-regular fa-circle-dot text-primary text-xl"></i>
+        </div>
+        <div class="timeline-end md:text-start mb-10">
+          <div class="text-lg font-black">First Macintosh computer</div>
+          The Apple Macintosh—later rebranded as the Macintosh 128K—is the original
+          Apple Macintosh personal computer. It played a pivotal role in establishing
+          desktop publishing as a general office function. The motherboard, a 9 in
+          (23 cm) CRT monitor, and a floppy drive were housed in a beige case with
+          integrated carrying handle; it came with a keyboard and single-button mouse.
+        </div>
+        <div class="timeline-start mb-10"></div>
+        <hr />
+      </li>
+
+      <!-- 4 -->
+
+      <li>
+        <div class="timeline-middle">
+          <i class="fa-regular fa-circle-dot text-primary text-xl"></i>
+        </div>
+        <div class="timeline-end md:text-start mb-10">
+          <div class="text-lg font-black">First Macintosh computer</div>
+          The Apple Macintosh—later rebranded as the Macintosh 128K—is the original
+          Apple Macintosh personal computer. It played a pivotal role in establishing
+          desktop publishing as a general office function. The motherboard, a 9 in
+          (23 cm) CRT monitor, and a floppy drive were housed in a beige case with
+          integrated carrying handle; it came with a keyboard and single-button mouse.
+        </div>
+        <div class="timeline-start mb-10"></div>
+        <hr />
+      </li>
+    </ul>
+
+    <h1 class="text-center font-bold text-5xl my-10">Jour 2</h1>
+    <ul
+      class="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical"
+    >
+      <!-- 1 -->
+      <li>
+        <div class="timeline-middle">
+          <i class="fa-regular fa-circle-dot text-primary text-xl"></i>
+        </div>
+        <div class="timeline-end mb-10">
+          <div class="text-lg font-black">Départ</div>
+          <p>
+            Départ en vélo à 8h15, direction Reims <br />
+            Bords de Marne jusqu'à la côte de Chessy pour me défoncer les jambes
+            <br />
+            Puis boue, il faisait frais et brumeux. <br />
+            Arrêt à Jouarre <br />
+            Monsieur St Jacques à croutes sur marne <br />
+            Fatigue musculaire <br />
+            Camping à Dormans (jeu de mot mdr) <br />
+          </p>
+        </div>
+        <div class="timeline-start mb-10">
+          <img src={un} alt="Macintosh" class="rounded-lg w-96" />
+        </div>
+        <hr />
+      </li>
+      <!-- 2 -->
+      <li>
+        <div class="timeline-middle">
+          <i class="fa-regular fa-circle-dot text-primary text-xl"></i>
+        </div>
+        <div class="timeline-end md:text-start mb-10">
+          <div class="text-lg font-black">First Macintosh computer</div>
+          The Apple Macintosh—later rebranded as the Macintosh 128K—is the original
+          Apple Macintosh personal computer. It played a pivotal role in establishing
+          desktop publishing as a general office function. The motherboard, a 9 in
+          (23 cm) CRT monitor, and a floppy drive were housed in a beige case with
+          integrated carrying handle; it came with a keyboard and single-button mouse.
+        </div>
+        <div class="timeline-start mb-10"></div>
+        <hr />
+      </li>
+
+      <!-- 3 -->
+
+      <li>
+        <div class="timeline-middle">
+          <i class="fa-regular fa-circle-dot text-primary text-xl"></i>
+        </div>
+        <div class="timeline-end md:text-start mb-10">
+          <div class="text-lg font-black">First Macintosh computer</div>
+          The Apple Macintosh—later rebranded as the Macintosh 128K—is the original
+          Apple Macintosh personal computer. It played a pivotal role in establishing
+          desktop publishing as a general office function. The motherboard, a 9 in
+          (23 cm) CRT monitor, and a floppy drive were housed in a beige case with
+          integrated carrying handle; it came with a keyboard and single-button mouse.
+        </div>
+        <div class="timeline-start mb-10"></div>
+        <hr />
+      </li>
+
+      <!-- 4 -->
+
+      <li>
+        <div class="timeline-middle">
+          <i class="fa-regular fa-circle-dot text-primary text-xl"></i>
+        </div>
+        <div class="timeline-end md:text-start mb-10">
+          <div class="text-lg font-black">First Macintosh computer</div>
+          The Apple Macintosh—later rebranded as the Macintosh 128K—is the original
+          Apple Macintosh personal computer. It played a pivotal role in establishing
+          desktop publishing as a general office function. The motherboard, a 9 in
+          (23 cm) CRT monitor, and a floppy drive were housed in a beige case with
+          integrated carrying handle; it came with a keyboard and single-button mouse.
+        </div>
+        <div class="timeline-start mb-10"></div>
+        <hr />
+      </li>
+    </ul>
+
+    <h1 class="text-center font-bold text-5xl my-10">Jour 3</h1>
+
+    <ul
+      class="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical"
+    >
+      <!-- 1 -->
+      <li>
+        <div class="timeline-middle">
+          <i class="fa-regular fa-circle-dot text-primary text-xl"></i>
+        </div>
+        <div class="timeline-end mb-10">
+          <div class="text-lg font-black">Départ</div>
+          <p>
+            Départ en vélo à 8h15, direction Reims <br />
+            Bords de Marne jusqu'à la côte de Chessy pour me défoncer les jambes
+            <br />
+            Puis boue, il faisait frais et brumeux. <br />
+            Arrêt à Jouarre <br />
+            Monsieur St Jacques à croutes sur marne <br />
+            Fatigue musculaire <br />
+            Camping à Dormans (jeu de mot mdr) <br />
+          </p>
+        </div>
+        <div class="timeline-start mb-10">
+          <img src={un} alt="Macintosh" class="rounded-lg w-96" />
+        </div>
+        <hr />
+      </li>
+      <!-- 2 -->
+      <li>
+        <div class="timeline-middle">
+          <i class="fa-regular fa-circle-dot text-primary text-xl"></i>
+        </div>
+        <div class="timeline-end md:text-start mb-10">
+          <div class="text-lg font-black">First Macintosh computer</div>
+          The Apple Macintosh—later rebranded as the Macintosh 128K—is the original
+          Apple Macintosh personal computer. It played a pivotal role in establishing
+          desktop publishing as a general office function. The motherboard, a 9 in
+          (23 cm) CRT monitor, and a floppy drive were housed in a beige case with
+          integrated carrying handle; it came with a keyboard and single-button mouse.
+        </div>
+        <div class="timeline-start mb-10"></div>
+        <hr />
+      </li>
+
+      <!-- 3 -->
+
+      <li>
+        <div class="timeline-middle">
+          <i class="fa-regular fa-circle-dot text-primary text-xl"></i>
+        </div>
+        <div class="timeline-end md:text-start mb-10">
+          <div class="text-lg font-black">First Macintosh computer</div>
+          The Apple Macintosh—later rebranded as the Macintosh 128K—is the original
+          Apple Macintosh personal computer. It played a pivotal role in establishing
+          desktop publishing as a general office function. The motherboard, a 9 in
+          (23 cm) CRT monitor, and a floppy drive were housed in a beige case with
+          integrated carrying handle; it came with a keyboard and single-button mouse.
+        </div>
+        <div class="timeline-start mb-10"></div>
+        <hr />
+      </li>
+
+      <!-- 4 -->
+
+      <li>
+        <div class="timeline-middle">
+          <i class="fa-regular fa-circle-dot text-primary text-xl"></i>
+        </div>
+        <div class="timeline-end md:text-start mb-10">
+          <div class="text-lg font-black">First Macintosh computer</div>
+          The Apple Macintosh—later rebranded as the Macintosh 128K—is the original
+          Apple Macintosh personal computer. It played a pivotal role in establishing
+          desktop publishing as a general office function. The motherboard, a 9 in
+          (23 cm) CRT monitor, and a floppy drive were housed in a beige case with
+          integrated carrying handle; it came with a keyboard and single-button mouse.
+        </div>
+        <div class="timeline-start mb-10"></div>
+        <hr />
+      </li>
+    </ul>
+  </div>
+
+  <h2>Jour 1</h2>
+
+  <h2>Jour 2</h2>
+  <p>Du camping vers Reims : full champs, tracteurs, gadoue totale, horrible</p>
+  <p>Retour : eau papy, pizza</p>
+  <h2>Jour 3</h2>
+  <p>
+    Route du Champagne : rue pleine de grandes maisons de riches vignerons <br
+    />
+    J'avais l'impression d'avoir fait un voyage de 70 ans dans le passé <br />
+    Petite ferme paumée au détour d'un village <br />
+    Plus d'eau : big soif <br />
+    Petite boutique près d'Ussy sur Marne : bière de Maux à défaut de Champagne
+    <br />
+    Plus batterie donc sans GPS <br />
+  </p>
   <!-- <div>
     {@html $markdownContent}
   </div> -->
@@ -43,5 +352,11 @@
 <style>
   #maindiv {
     margin: 0 10%;
+  }
+
+  #video1 {
+    width: calc(100% - 20rem);
+    margin: 0 10rem;
+    margin-bottom: 3rem;
   }
 </style>
