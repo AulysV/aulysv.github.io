58ee2e5270942fa9e9860b160bd310b73f8b97e9
Aulys VINAY <aulys.n.vinay@outlook.com>
2024-11-03 14:09:55 +0100

    Update accueil

---
+++ aulys/src/routes/apropo/+page.svelte
@@ -0,0 +1,31 @@
+<div class="blur-bg w-screen"></div>
+
+<div id="content" class="w-screen">
+  <h1 class="text-5xl font-bold mt-32 text-center">Me</h1>
+  <p class="text-center">(Je - Ich - Ego)</p>
+  <div class="divider mx-10">
+    <i class="fa-solid fa-minus text-3xl text-primary"></i>
+  </div>
+
+  <article
+    id="maindiv"
+    class="prose max-w-none lg:text-xl lg:text-justify md:text-justify"
+  >
+    <p>Gros gloubi-boulga sur moi, ne pas le prendre comme un CV :</p>
+    <p></p>
+  </article>
+</div>
+
+<style>
+  #maindiv {
+    margin: 0 10%;
+  }
+
+  #saperlipopette {
+    border-top: 3px solid rgba(0, 0, 0, 0.4);
+    border-bottom: 3px solid rgba(0, 0, 0, 0.4);
+    background: rgba(43, 43, 43, 0.137);
+    backdrop-filter: blur(80px);
+    margin-bottom: 2.5em;
+  }
+</style>
